e7c17a63350a338e7af1e23ca8a7913a25e9c38d
sandrofunk <sagfunk@gmail.com>
2021-03-28 11:25:23 -0300

    Casting - Ex2

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double a;
+		int b;
+		
+		a = 5.0;
+		
+		//casting
+		b = (int)a;
+		
+		System.out.println(b);
+
+	}
+
+}
